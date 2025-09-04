c1a5971d58061eaf4821e13239c43895a06037e4
Beverly Mugwadi <b.mugwadi1@alustudent.com>
2025-09-04 13:19:31 +0200

    concat

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Type-annotated function add"""
 
+
 def add(a: float, b: float) -> float:
     """Return the sum of a and b as a float."""
     return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function concat"""
+
+
+def concat(a: str, b: str) -> str:
+    """Return a concatenated with b as a str."""
+    return a + b
