8c222f6537469ae5266527cca43f72d3772884a5
Beverly Mugwadi <b.mugwadi1@alustudent.com>
2025-10-26 12:20:38 +0200

    correction

--- ES6_basic/0-constants.test.js
+++ ES6_basic/0-constants.test.js
@@ -1,9 +1,13 @@
-import { taskFirst, taskNext } from './0-constants.js';
+import { taskFirst, taskNext } from './0-constants';
 
-test('taskFirst should return correct string', () => {
-  expect(taskFirst()).toBe('I prefer const when I can.');
-});
+describe('task functions tests', () => {
+  it('taskFirst should return correct string', () => {
+    expect.assertions(1);
+    expect(taskFirst()).toBe('I prefer const when I can.');
+  });
 
-test('taskNext should return correct string', () => {
-  expect(taskNext()).toBe('But sometimes let is okay');
+  it('taskNext should return correct string', () => {
+    expect.assertions(1);
+    expect(taskNext()).toBe('But sometimes let is okay');
+  });
 });
